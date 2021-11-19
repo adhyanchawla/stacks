2293fb073b4798a2f1d772f9b7e13898ec6092d4
imadhyan <adhyanchawla500@gmail.com>
2021-11-20 00:13:11 +0530

    commit

--- questions.java
+++ questions.java
@@ -251,6 +251,146 @@ public class questions {
     }
 
     //lc 856
+    public int scoreOfParentheses(String s) {
+        int n = s.length();
+        LinkedList<Integer>st = new LinkedList<>();
+        st.addFirst(0);
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(ch == '(') {
+                st.addFirst(0);
+            } else {
+                int a = st.removeFirst();
+                int b = st.removeFirst();
+                
+                int val = b + Math.max(2 * a, 1);
+                st.addFirst(val);
+            }
+        }
+        return st.removeFirst();
+    }
 
+    //lc 921
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        LinkedList<Character>st = new LinkedList<>();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(ch == '(') {
+                st.addFirst(ch);
+            } else {
+                while(st.size() != 0 && st.getFirst() != '(') {
+                    st.removeFirst();
+                }
+                
+                if(st.size() != 0 && st.getFirst() == '(') {
+                    st.removeFirst();
+                } else count++;
+            }
+        }
+        
+        return st.size() + count;
+    }
+
+    // 84
+    //7n
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        //nsor
+        int[] ans1 = new int[n];
+        Arrays.fill(ans1, n);
+        
+        LinkedList<Integer>st1 = new LinkedList<>();
+        st1.addFirst(-1);
+        
+        for(int i = 0; i < n; i++) {
+            while(st1.getFirst() != -1 && heights[st1.getFirst()] > heights[i]) {
+                ans1[st1.removeFirst()] = i;
+            }
+            
+            st1.addFirst(i);
+        }
+        
+        //nsol
+        int[] ans2 = new int[n];
+        Arrays.fill(ans2, -1);
+        
+        LinkedList<Integer>st2 = new LinkedList<>();
+        st2.addFirst(-1);
+        
+        for(int i = n - 1; i >= 0; i--) {
+            while(st2.getFirst() != -1 && heights[st2.getFirst()] > heights[i]) {
+                ans2[st2.removeFirst()] = i;
+            }
+            
+            st2.addFirst(i);
+        }
+        
+        int maxArea = 0;
+        for(int i = 0; i < n; i++) {
+            maxArea = Math.max(maxArea, heights[i] * (ans1[i] - ans2[i] - 1));
+        }
+        
+        return maxArea;
+    }
+
+    //2n
+    public int largestRectangleArea_01(int[] heights) {
+        LinkedList<Integer>st = new LinkedList<>();
+        st.addFirst(-1);
+        int maxArea = 0, n = heights.length;
+        
+        for(int i = 0; i < n; i++) {
+            while(st.getFirst() != -1 && heights[st.getFirst()] > heights[i]) {
+                int h = heights[st.removeFirst()];
+                int w = i - st.getFirst() - 1;
+                maxArea = Math.max(maxArea, h * w);
+            }
+            
+            st.addFirst(i);
+        }
+        
+        while(st.getFirst() != -1) {
+                int h = heights[st.removeFirst()];
+                int w = n - st.getFirst() - 1;
+                maxArea = Math.max(maxArea, h * w);
+        }
+        
+        return maxArea;       
+    }
+
+    //85
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length == 0) return 0;
+        int n = matrix.length, m = matrix[0].length;
+        
+        int[] height = new int[m];
+        int maxArea = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                height[j] = matrix[i][j] == '0' ? 0 : height[j] + 1;
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(height));
+        }
+        return maxArea;
+    }
+
+    //32
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        LinkedList<Integer>st = new LinkedList<>();
+        st.addFirst(-1);
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(ch == ')' && st.getFirst() != -1 && s.charAt(st.getFirst()) == '(') {
+                st.removeFirst();
+                max = Math.max(max, i - st.getFirst());
+            }
+            else st.addFirst(i);
+        }
+        return max;
+    }
 
 }
